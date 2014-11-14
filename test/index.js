639be9bf5114a8eea594b1a60b7811afda5814c6
Garrett Johnson <garrett@segment.io>
2014-11-14 08:46:02 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,92 @@
+
+var Test = require('segmentio-integration-tester');
+var helpers = require('./helpers');
+var facade = require('segmentio-facade');
+var should = require('should');
+var assert = require('assert');
+var Klaviyo = require('..');
+
+describe('Klaviyo', function () {
+  var settings;
+  var klaviyo;
+  var test;
+
+  beforeEach(function(){
+    settings = { apiKey: 'eHdVzM' };
+    klaviyo = new Klaviyo(settings);
+    test = Test(klaviyo, __dirname);
+  });
+
+  it('should have correct settings', function(){
+    test
+      .name('Klaviyo')
+      .endpoint('http://a.klaviyo.com/api')
+      .ensure('settings.apiKey')
+      .channels(['server'])
+      .retries(2);
+  });
+
+  describe('.validate()', function () {
+    it('should be invalid when .apiKey is missing', function(){
+      delete settings.apiKey;
+      test.invalid({}, settings);
+    });
+
+    it('should be valid when given complete settings', function(){
+      test.valid({}, settings);
+    });
+  });
+
+  describe('mapper', function(){
+    describe('identify', function(){
+      it('should map basic identify', function(){
+        test.maps('identify-basic', settings);
+      });
+
+      it('should fallback to anonymousId', function(){
+        test.maps('identify-anonymous-id', settings);
+      });
+    });
+
+    describe('track', function(){
+      it('should map basic track', function(){
+        test.maps('track-basic', settings);
+      });
+    });
+  });
+
+  describe('.track()', function () {
+    it('should be able to track correctly', function(done){
+      test
+        .set(settings)
+        .track(helpers.track())
+        .expects(200)
+        .end(done);
+    });
+
+    it('should error on invalid response', function(done){
+      test
+        .set({ apiKey: null })
+        .track(helpers.track())
+        .error('Klaviyo: bad response', done);
+    });
+  });
+
+
+  describe('.identify()', function () {
+    it('should be able to identify correctly', function(done){
+      test
+        .set(settings)
+        .identify(helpers.identify())
+        .expects(200)
+        .end(done);
+    });
+
+    it('should error on invalid response', function(done){
+      test
+        .set({ apiKey: null })
+        .identify(helpers.identify())
+        .error('Klaviyo: bad response', done);
+    });
+  });
+});
